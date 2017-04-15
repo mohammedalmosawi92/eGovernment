ee92eb3ef0e74d051b757c64ad0bbd8413aca26c
mohammedalmosawi92 <messibarcelona92@gmail.com>
2017-04-15 12:45:55 +0300

    connected the backend with the frontend

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var config = require("./config");
+var ejs = require("ejs");
+var path = require("path");
+var config = require("./config.js");
 
 //setup port
 var port = process.env.Port || 8080
@@ -10,19 +13,27 @@ var port = process.env.Port || 8080
 var app = express();
 
 //connect to mongodb database
-mongoose.connect("mongodb://localhost/");
+mongoose.connect("mongodb://localhost/" + config.database);
 
-//setup sever to handle json
+//setup server to handle json
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+//setyp server to handle html
+app.use(express.static(path.join(__dirname + "\\..\\public\\")));
+app.set("views", __dirname + "\\..\\public\\views");
+app.engine("html", ejs.renderFile);
+app.set("view engine", "ejs");
 
 //import routes
 var apiRouter = require("./Routes/api.js");
 var authRouter = require("./Routes/auth.js");
+var filesRouter = require("./Routes/files.js");
 
 //use routes
-app.use("/personal", apiRouter);
 app.use(authRouter);
+app.use("/personal", apiRouter);
+app.use(filesRouter);
 
 //listen to port
 app.listen(port, function() {
