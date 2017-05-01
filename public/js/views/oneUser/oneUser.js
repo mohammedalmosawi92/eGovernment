29309d9759bb4c734a1b1ebad1cf3f7ced85cdb9
mohammedalmosawi92 <messibarcelona92@gmail.com>
2017-05-02 00:03:23 +0300

    styled oneUser and allData pages

--- public/js/views/oneUser/oneUser.js
+++ public/js/views/oneUser/oneUser.js
@@ -9,22 +9,45 @@ app.config(function ($routeProvider) {
 
 app.controller("oneUserCtrl", function ($scope, requestService, usersService, $routeParams, tempService) {
     var userId = $routeParams.id;
+    $scope.websiteDatabase = false;
     $scope.loadData = function () {
         requestService.getDataByUserId(userId).then(function (response) {
             if (response.data.data) {
                 $scope.item = response.data.data;
+                $scope.websiteDatabase = true;
+                $scope.item.edit = false;
+                $scope.item.dob = new Date($scope.item.dob);
             } else {
                 tempService.getDataByUserId(userId).then(function (response) {
                     $scope.item = response.data.data;
+                    $scope.item.edit = false
                 })
             }
         }, function (response) {
             console.log("no data");
         })
-        usersService.getUserById(userId).then(function(response) {
+        usersService.getUserById(userId).then(function (response) {
             $scope.reqList = response.data.data.requests;
-        }, function(response) {
+        }, function (response) {
             console.log(response.status);
+        })
+    }
+
+    $scope.edit = function () {
+        $scope.item.edit = !$scope.item.edit;
+    }
+
+    $scope.saveToDB = function (id, item) {
+        $scope.item.edit = !$scope.item.edit;
+        requestService.updateData(id, item).then(function() {
+            $scope.loadData();
+        })
+    }
+
+    $scope.saveToTemp = function(id, item) {
+        $scope.item.edit = !$scope.item.edit;
+        tempService.updateData(id, item).then(function() {
+            $scope.loadData();
         })
     }
 })
